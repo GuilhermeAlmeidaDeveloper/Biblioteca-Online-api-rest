c2bcb1f93c8540f067f6755cdcfdfdcdb62cd80f
Guilherme Almeida <guilherme.dev.almeida@gmail.com>
2023-04-12 22:02:28 -0300

    Delete and Patch implementados

--- src/controladores/livros.js
+++ src/controladores/livros.js
@@ -54,11 +54,46 @@ const substituirLivro = (req, res) => {
     return res.json({ mensagem: "Livro substituído." })
 }
 
+const alterarLivro = (req, res) => {
+    const { id } = req.params
+    const { titulo, autor, ano, numPaginas } = req.body
+
+
+    let livro = livros.find((livro) => {
+        return livro.id === Number(id)
+    })
+
+    if (!livro) {
+        return res.status(400).send({ mensagem: 'O ID do livro não existe.' });
+    }
+
+
+    livro.titulo = titulo ?? livro.titulo
+    livro.autor = autor ?? livro.autor
+    livro.ano = ano ?? livro.ano
+    livro.numPaginas = numPaginas ?? livro.numPaginas
+
+    return res.json({ mensagem: "Livro alterado." })
+}
+
+const deletarLivro = (req, res) => {
+    const { id } = req.params
+
+    livros = livros.filter((livro) => {
+        return livro.id !== Number(id)
+    })
+
+
+    return res.json({ mensagem: "Livro removido." })
+
+}
 
 
 module.exports = {
     consultarLivros,
     consultarLivro,
     adicionarLivro,
-    substituirLivro
+    substituirLivro,
+    alterarLivro,
+    deletarLivro
 }
--- src/rotas.js
+++ src/rotas.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const rotas = express()
-const { consultarLivros, consultarLivro, adicionarLivro, substituirLivro } = require('./controladores/livros')
+const { consultarLivros, consultarLivro, adicionarLivro, substituirLivro, alterarLivro, deletarLivro } = require('./controladores/livros')
 
 rotas.get('/livros', consultarLivros)
 rotas.get('/livros/:id', consultarLivro)
 rotas.post('/livros/', adicionarLivro)
 rotas.put('/livros/:id', substituirLivro)
+rotas.patch('/livros/:id', alterarLivro)
+rotas.delete('/livros/:id', deletarLivro)
 
 module.exports = rotas
